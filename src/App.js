3c2fe17ae29249bdd03f9d90550d8147e31c246e
Miguel Cobas <mcobas@dealergeek.com>
2020-04-18 22:49:38 -0500

    Se agrego Lista de imagenes y imagen contenedor con sus detalles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, {useState, useEffect} from 'react';
 import Formulario from './components/Formulario'
+import ListadoImagenes from './components/ListadoImagenes'
 
 
 function App() {
   const [ busqueda, guardarBusqueda] = useState('');
+  const [ imagenes, guardarImagenes] = useState([]);
 
 
   useEffect(()=>{
@@ -15,7 +17,7 @@ function App() {
 
     const respuesta = await fetch(url)
     const resultado = await respuesta.json();
-    guardarBusqueda(resultado.hits)
+    guardarImagenes(resultado.hits)
 
    }
    consultarApi()
@@ -23,12 +25,17 @@ function App() {
   }, [busqueda])
   return (
     <div className="container">
-    <div className="jumbotron">
-      <p className="lead text-center">Buscador de imagenes</p>
-      <Formulario
-        guardarBusqueda={guardarBusqueda}
-       />
-    </div>
+      <div className="jumbotron">
+          <p className="lead text-center">Buscador de imagenes</p>
+          <Formulario
+            guardarBusqueda={guardarBusqueda}
+          />
+      </div>
+      <div className="row justify-content-center">
+        <ListadoImagenes 
+        imagenes={imagenes}
+        />
+      </div>
     </div>
   );
 }
